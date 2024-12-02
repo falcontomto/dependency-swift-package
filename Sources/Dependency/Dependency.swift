77f45a9de0ade076e9d1f331aa9c2c52901584cc
Yat To <yat.to@incrowdsports.com>
2024-12-02 17:56:10 +0000

    propertyWrapper for convenience

--- Sources/Dependency/Dependency.swift
+++ Sources/Dependency/Dependency.swift
@@ -1,2 +1,28 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Denpendency.swift
+//
+//
+//  Created by Yat To on 03/02/2024.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct Dependency<Value> {
+    private let keyPath: KeyPath<DependencyContainer, Value>
+    private let container: DependencyContainer
+    
+    public init(
+        _ keyPath: KeyPath<DependencyContainer, Value>,
+        container: DependencyContainer = DependencyContainer.shared
+    ) {
+        self.keyPath = keyPath
+        self.container = container
+    }
+    
+    public var wrappedValue: Value {
+        container[keyPath: keyPath]
+    }
+}
+
+extension Dependency: @unchecked Sendable where Value: Sendable {}
